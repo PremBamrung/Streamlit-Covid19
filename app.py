2be2b648da0c414b66377ef9a54485290059c06e
PremBamrung <misterpremsimson@hotmail.fr>
2020-03-21 12:41:00 +0100

    Adding more functionning sidebar, elarge fig size and other functionnality

--- app.py
+++ app.py
@@ -31,21 +31,77 @@ def load_data():
     return df3
 
 st.title('Dashboard Covid19')
+st.write("This is an experimental dashboard to showcase the evolution of Covid-19 around the world.")
 
+with st.spinner('Loading data...'):
+    df=load_data()
 
-# st.header("Loading data")
-df=load_data()
 
 features=["Confirmed","Dead","Recovered","Active"]
 
+# Sidebar 
+
+st.sidebar.title('Tool Bar')
+mode=st.sidebar.selectbox("Chose your chapter :",["Intro","Overview","Country","Map","Flourish","About"])
 
 
-mode=st.sidebar.selectbox("Mode",["Overview","Flourish","About"])
+st.sidebar.subheader("Summary :")
+temp = df.groupby('Date')['Confirmed', 'Dead', 'Recovered', 'Active'].sum().reset_index()
+temp = temp[temp['Date']==max(temp['Date'])].reset_index(drop=True)
+st.sidebar.dataframe(temp[["Confirmed","Dead","Recovered","Active"]].style.background_gradient(cmap='Pastel1'))
 
 
-if mode=="Overview":
+# horizontal bar
+# fig=go.Figure(data=[
+#     go.Bar(name="Active",y=temp.index,x=temp.Active,orientation="h"),
+#     go.Bar(name="Dead",y=temp.index,x=temp.Dead,orientation="h"),
+#     go.Bar(name="Recovered",y=temp.index,x=temp.Recovered,orientation="h")
+# ])
 
-    st.write("This is an experimental dashboard to showcase the evolution of Covid-19 around the world.")
+# fig.update_layout(
+# # yaxis_title="Cases",
+# # xaxis_title="Cases",
+# showlegend=True,
+# barmode='stack',
+# hovermode='x'
+# ,height=300, width=300,
+# margin=dict(l=0, r=0, t=5, b=0))
+
+tm = temp.melt(id_vars="Date", value_vars=['Active', 'Dead', 'Recovered'])
+
+fig=px.pie(tm,values='value', names='variable',height=300, width=300)
+fig.update_layout(margin=dict(l=0, r=0, t=0, b=0),showlegend=False,paper_bgcolor="#F4F5F7")
+fig.update_traces(textposition='inside', textinfo='percent+label')
+
+st.sidebar.plotly_chart(fig)
+
+
+st.sidebar.subheader("How to ? ")
+
+st.sidebar.markdown("""
+***Confirmed*** cases are people tested positive. Include all cases.
+
+***Recovered*** cases are people reported as healed.
+
+***Active*** = Confirmed - Recovered - Dead ==> people still infected but neither dead nor recovered
+
+You can **interact** with each graphs (zooming, hovering, saving ...)
+
+
+Data are taken from the data repository of the *Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE)*
+""")
+st.sidebar.markdown("Last updated on 21/03/2020")
+st.sidebar.markdown("Created on 18/03/2020")
+
+
+
+if mode=="Intro":
+    st.header("This chapter is in construction ... More to come soon. You can still see the over chapter on the Tool Bar (left).")
+
+
+elif mode =='Overview':
+
+    
 
     if st.checkbox("Show raw data"):
         nb_head = st.slider('Number of rows to show',min_value=5,max_value=1000)
@@ -56,8 +112,6 @@ if mode=="Overview":
     st.header("Global")
 
     carotte=df.groupby('Date')[features].sum()
-
-
 
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=carotte.index, y=carotte.Confirmed,
@@ -74,23 +128,38 @@ if mode=="Overview":
     fig.add_trace(go.Scatter(x=carotte.index, y=carotte.Active,
                     mode='lines+markers',
                     name='Active'))    
-
+    fig.update_layout(height=700, width=900)
     st.plotly_chart(fig)
 
-    toto=df[df.Date=="2020-03-19"].groupby("Country/Region")[features].sum().sort_values("Confirmed",ascending=False)
+    toto=df[df.Date==max(df['Date'])].groupby("Country/Region")[features].sum().sort_values("Confirmed",ascending=False)
     toto=toto.head(20)
     # fig=px.bar(toto,x=toto.index,y="Confirmed",orientation="v",color=toto.index)
     fig=go.Figure(data=[
         go.Bar(name="Active",x=toto.index,y=toto.Active),
         go.Bar(name="Dead",x=toto.index,y=toto.Dead),
         go.Bar(name="Recovered",x=toto.index,y=toto.Recovered),
+        
+
+        
     ])
 
     fig.update_layout(
     yaxis_title="Cases",
     xaxis_title="Country",
     showlegend=True,
-    barmode='stack')
+    barmode='stack',
+    hovermode='x',
+    height=700, width=900)
+
+    st.plotly_chart(fig)
+
+# Treemap
+
+    temp = df.groupby('Date')['Confirmed', 'Dead', 'Recovered', 'Active'].sum().reset_index()
+    temp = temp[temp['Date']==max(temp['Date'])].reset_index(drop=True)
+    tm = temp.melt(id_vars="Date", value_vars=['Active', 'Dead', 'Recovered'])
+    fig = px.treemap(tm, path=["variable"], values="value", height=700, width=900)
+
     st.plotly_chart(fig)
 
 
@@ -110,7 +179,8 @@ if mode=="Overview":
     fig.update_layout(
         title=f"Evolution of cases for {selected_country}",
         xaxis_title="Time",
-        yaxis_title="Population")
+        yaxis_title="Population",
+        height=700, width=900)
     st.plotly_chart(fig)
 
 
@@ -122,13 +192,14 @@ if mode=="Overview":
 
     st.header(f"Total Confirmed : {df_last.Confirmed[0]}")
 
-    toto=df[df.Date=="2020-03-19"].groupby("Country/Region")[features].sum().sort_values("Confirmed",ascending=False)
+    toto=df[df.Date==max(df['Date'])].groupby("Country/Region")[features].sum().sort_values("Confirmed",ascending=False)
     toto=toto.head(15)
     fig=px.bar(toto,y=toto.index,x="Confirmed",orientation="h",color=toto.index).update_yaxes(categoryorder="total ascending")
     fig.update_layout(
     xaxis_title="Confirmed Cases",
     yaxis_title="Country",
-    showlegend=False)
+    showlegend=False,
+    height=700, width=900)
     st.plotly_chart(fig)
 
 
@@ -136,13 +207,14 @@ if mode=="Overview":
 
     st.header(f"Total Death : {df_last.Dead[0]} ({round((df_last.Dead[0]/df_last.Confirmed[0])*100,2)} %)")
 
-    toto=df[df.Date=="2020-03-19"].groupby("Country/Region")[features].sum().sort_values("Dead",ascending=False)
+    toto=df[df.Date==max(df['Date'])].groupby("Country/Region")[features].sum().sort_values("Dead",ascending=False)
     toto=toto.head(15)
     fig=px.bar(toto,y=toto.index,x="Dead",orientation="h",color=toto.index).update_yaxes(categoryorder="total ascending")
     fig.update_layout(
     xaxis_title="Dead Cases",
     yaxis_title="Country",
-    showlegend=False)
+    showlegend=False,
+    height=700, width=900)
     st.plotly_chart(fig)
 
 
@@ -150,13 +222,14 @@ if mode=="Overview":
 
     st.header(f"Total Recovered : {df_last.Recovered[0]} ({round((df_last.Recovered[0]/df_last.Confirmed[0])*100,2)} %)")
 
-    toto=df[df.Date=="2020-03-19"].groupby("Country/Region")[features].sum().sort_values("Recovered",ascending=False)
+    toto=df[df.Date==max(df['Date'])].groupby("Country/Region")[features].sum().sort_values("Recovered",ascending=False)
     toto=toto.head(15)
     fig=px.bar(toto,y=toto.index,x="Recovered",orientation="h",color=toto.index).update_yaxes(categoryorder="total ascending")
     fig.update_layout(
     xaxis_title="Recovered Cases",
     yaxis_title="Country",
-    showlegend=False)
+    showlegend=False,
+    height=700, width=900)
     st.plotly_chart(fig)
 
 
@@ -164,40 +237,44 @@ if mode=="Overview":
 
     st.header(f"Total Active : {df_last.Active[0]} ({round((df_last.Active[0]/df_last.Confirmed[0])*100,2)} %)")
 
-    toto=df[df.Date=="2020-03-19"].groupby("Country/Region")[features].sum().sort_values("Active",ascending=False)
+    toto=df[df.Date==max(df['Date'])].groupby("Country/Region")[features].sum().sort_values("Active",ascending=False)
     toto=toto.head(15)
     fig=px.bar(toto,y=toto.index,x="Active",orientation="h",color=toto.index).update_yaxes(categoryorder="total ascending")
     fig.update_layout(
     xaxis_title="Active Cases",
     yaxis_title="Country",
-    showlegend=False)
+    showlegend=False,
+    height=700, width=900)
     st.plotly_chart(fig)
 
 
 
     st.header(f"DataFrame")
-    kiwi= df[df.Date=="2020-03-19"].groupby("Country/Region")[features].sum()
+    kiwi= df[df.Date==max(df['Date'])].groupby("Country/Region")[features].sum()
     kiwi["Recovery Rate"]=round(kiwi.Recovered/kiwi.Confirmed,2)
     kiwi["Death Rate"]=round(kiwi.Dead/kiwi.Confirmed,2)
     kiwi["Death Rate"]=kiwi["Death Rate"].map(lambda n: '{:.2%}'.format(n))
     kiwi["Recovery Rate"]=kiwi["Recovery Rate"].map(lambda n: '{:.2%}'.format(n))
-    st.dataframe(kiwi.style.highlight_max(axis=0))
+    st.dataframe(kiwi.style.background_gradient(cmap='Reds'))
 
+elif mode =='Country':
+    st.header("This chapter is in construction ... More to come soon. You can still see the over chapter on the Tool Bar (left).")
 
-
+elif mode =='Map':
+    st.header("This chapter is in construction ... More to come soon. You can still see the over chapter on the Tool Bar (left).")
 
 
 elif mode =='Flourish':
     st.header("Evolution of confirmed cases around the world")
-    flourish_confirmed="<iframe src='https://public.flourish.studio/visualisation/1627982/embed' frameborder='0' scrolling='no' style='width:100%;height:600px;'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/1627982/?utm_source=embed&utm_campaign=visualisation/1627982' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>"
+    flourish_confirmed="<iframe src='https://public.flourish.studio/visualisation/1627982/embed' frameborder='0' scrolling='no' style='width:150%;height:600px;'></iframe><div style='width:150%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/1627982/?utm_source=embed&utm_campaign=visualisation/1627982' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>"
     st.markdown(flourish_confirmed, unsafe_allow_html=True)
 
     st.header("Evolution of recovered cases around the world")
-    flourish_recovered="<iframe src='https://public.flourish.studio/visualisation/1628304/embed' frameborder='0' scrolling='no' style='width:100%;height:600px;'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/1628304/?utm_source=embed&utm_campaign=visualisation/1628304' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>"
+    flourish_recovered="<iframe src='https://public.flourish.studio/visualisation/1628304/embed' frameborder='0' scrolling='no' style='width:150%;height:600px;'></iframe><div style='width:150%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/1628304/?utm_source=embed&utm_campaign=visualisation/1628304' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>"
     st.markdown(flourish_recovered, unsafe_allow_html=True)
     
     st.header("Evolution of death cases around the world")
-    flourish_death="<iframe src='https://public.flourish.studio/visualisation/1628288/embed' frameborder='0' scrolling='no' style='width:100%;height:600px;'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/1628288/?utm_source=embed&utm_campaign=visualisation/1628288' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>"
+    flourish_death="<iframe src='https://public.flourish.studio/visualisation/1628288/embed' frameborder='0' scrolling='no' style='width:150%;height:600px;'></iframe><div style='width:150%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/1628288/?utm_source=embed&utm_campaign=visualisation/1628288' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>"
     st.markdown(flourish_death, unsafe_allow_html=True)
 
 
@@ -209,12 +286,12 @@ elif mode =='Flourish':
 elif mode=="About":
 
     st.write("""
-    Hi, I'm Premchanok BAMRUNG, currently intern Data Scientist. I was working on some data for my company when I thought that it would be a cool idea to use Streamlit to showcase my Exploratory Dat Analysis. But then came the Covid 19 crisi and I thought that it would be cool to create another dashboard on my own.
+    Hi, I'm Premchanok BAMRUNG, currently intern Data Scientist. I was working on some data for my company when I thought that it would be a cool idea to use Streamlit to showcase my Exploratory Data Analysis. But then came the Covid 19 crisis and I thought that it would be cool to create another dashboard on my own to monitor cases.
     
     The idea is to use Streamlit, Heroku, the standard Data Science stack (Pandas, Numpy, Seaborn...) and Flourish to create a really cool dashboard where one can select 
     whatever to see.
 
-    If you have any suggestion, do not hesitate to contact me.
+    If you have any suggestions or questions, do not hesitate to contact me.
 
 
     Contact: bamrungprem@gmail.com
